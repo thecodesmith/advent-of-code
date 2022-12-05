9747609db7fcbe490af0b1db1f6836c8c6d69004
Brian Stewart <brian.stewart@jamf.com>
2022-12-05 15:36:49 -0600

    Solve day 3 part 2 in Groovy

--- 2022/03/groovy/day03.groovy
+++ 2022/03/groovy/day03.groovy
@@ -1,32 +1,59 @@
 in = new Scanner(System.in)
 
-totalPriority = 0
-
 lowerBaseline = ascii('a')
 upperBaseline = ascii('A')
 
-warnings = 0
+// println 'Part 1'
+// part1()
 
-while (in.hasNextLine()) {
-    line = in.nextLine()
-    int mid = line.size() / 2
+println 'Part 2'
+part2()
 
-    def compartment1 = line.substring(0, mid).toList()
-    def compartment2 = line.substring(mid).toList()
-    def items = compartment1.intersect(compartment2).toSet()
-    def p = priority(items.first())
+def part1() {
+    totalPriority = 0
+    warnings = 0
 
-    totalPriority += p
+    while (in.hasNextLine()) {
+        line = in.nextLine()
+        int mid = line.size() / 2
 
-    println "shared: $items $p - $compartment1  $compartment2"
+        def compartment1 = line.substring(0, mid).toList()
+        def compartment2 = line.substring(mid).toList()
+        def items = compartment1.intersect(compartment2).toSet()
+        def p = priority(items.first())
 
-    if (items.size() > 1) {
-        println '  WARNING: multiple items'
-        warnings++
+        totalPriority += p
+
+        println "shared: $items $p - $compartment1  $compartment2"
+
+        if (items.size() > 1) {
+            println '  WARNING: multiple items'
+            warnings++
+        }
     }
+
+    println "total: $totalPriority ($warnings warnings)"
 }
 
-println "total: $totalPriority ($warnings warnings)"
+def part2() {
+
+    totalPriority = 0
+
+    while (in.hasNextLine()) {
+        def elf1 = in.nextLine()
+        def elf2 = in.nextLine()
+        def elf3 = in.nextLine()
+
+        def badge = elf1.toList().intersect(elf2.toList()).intersect(elf3.toList()).first()
+
+        def p = priority(badge)
+        totalPriority += p
+
+        println "badge: $badge, priority: $p"
+    }
+
+    println "total: $totalPriority"
+}
 
 def priority(item) {
     switch (item) {
